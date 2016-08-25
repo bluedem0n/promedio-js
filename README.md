03ea217f63f2adde64cb5df9c8449228b9f646dc
Sthefany Floriano <sthefanyflorianog@gmail.com>
2016-08-25 15:45:45 -0500

    Subiendo Ejercicio2

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # JS-Ejercicio2
+
+Procedimiento de promediar 3 numeros con Javascript
+1. Inicio
+2. Leer Datos
+* Dato 1
+* Dato 2
+* Dato 3
+3. Promediar datos
+4. Entregar promedio
+5. Fin
+
+![]("http://i65.tinypic.com/312yfli.jpg"diagrama"")
---
+++ app.js
@@ -0,0 +1,5 @@
+var numero1 = prompt ("Promediando con Sthef : Ingresa el primer numero");
+var numero2 = prompt ("Promediando con Sthef : Ingresa el segundo numero");
+var numero3 = prompt ("Promediando con Sthef : Ingresa el tercer numero");
+var promedio = (parseInt(numero1) + parseInt(numero2) + parseInt(numero3)) / 3;
+    window.alert("Tu resultado es : " + promedio);
